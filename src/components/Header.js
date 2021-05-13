968973890f5ddae0a9251154fb2ef62a540fdd6e
Filin1985 <ihsmen@gmail.com>
2021-05-13 18:54:59 +0300

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <img
+        src="https://api.freelogodesign.org/assets/thumb/logo/11106732_400.png"
+        alt=""
+      />
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/components/Profile.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+const Profile = () => {
+  return (
+    <div className="content">
+      <div>
+        <img
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTThMRccW6KuPopbxyNYFX7qwLt0xwFT5oBWg&usqp=CAU"
+          alt=""
+        />
+      </div>
+      <div>ava + description</div>
+      <div>
+        My posts
+        <div>New post</div>
+        <div>
+          <div>Post 1</div>
+          <div>Post 2</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
